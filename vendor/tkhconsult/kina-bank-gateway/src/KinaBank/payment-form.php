c040a7143d54bae1c0300ea3b64ec9b927d51142
kch <wilzokch@gmail.com>
2020-11-28 18:15:49 +0800

    added cache header to payment form

--- vendor/tkhconsult/kina-bank-gateway/src/KinaBank/payment-form.php
+++ vendor/tkhconsult/kina-bank-gateway/src/KinaBank/payment-form.php
@@ -8,6 +8,9 @@
      * @var bool $autoSubmit
      */
 ?>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
 <script src="https://devegateway.kinabank.com.pg/kina/js/kbl-ec.js"></script>
 <link href="https://devegateway.kinabank.com.pg/kina/css/kbl-ec.css" rel="stylesheet">
 <form
