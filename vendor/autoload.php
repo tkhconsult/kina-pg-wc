430f5f5493b06656ab3784d70d4cda6aff5904c6
kch <wilzokch@gmail.com>
2020-12-26 00:43:35 +0800

    fixed crash name autoloader with victoria bank plugin

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit50478dfc75549edea87a587529262f06::getLoader();
+return ComposerAutoloaderInit50478dfc75549edea87a587529262f05::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit50478dfc75549edea87a587529262f06
+class ComposerStaticInit50478dfc75549edea87a587529262f05
 {
     public static $prefixLengthsPsr4 = array (
         'T' => 
@@ -20,28 +20,11 @@ class ComposerStaticInit50478dfc75549edea87a587529262f06
         ),
     );
 
-    public static $classMap = array (
-        'TkhConsult\\KinaBankGateway\\KinaBankGateway' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBankGateway.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Authorization\\AuthorizationRequest' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Authorization/AuthorizationRequest.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Authorization\\AuthorizationResponse' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Authorization/AuthorizationResponse.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Completion\\CompletionRequest' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Completion/CompletionRequest.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Completion\\CompletionResponse' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Completion/CompletionResponse.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Exception' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Exception.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Form' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Form.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Request' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Request.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\RequestInterface' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/RequestInterface.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Response' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Response.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\ResponseInterface' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/ResponseInterface.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Reversal\\ReversalRequest' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Reversal/ReversalRequest.php',
-        'TkhConsult\\KinaBankGateway\\KinaBank\\Reversal\\ReversalResponse' => __DIR__ . '/..' . '/tkhconsult/kina-bank-gateway/src/KinaBank/Reversal/ReversalResponse.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit50478dfc75549edea87a587529262f06::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit50478dfc75549edea87a587529262f06::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit50478dfc75549edea87a587529262f06::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit50478dfc75549edea87a587529262f05::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit50478dfc75549edea87a587529262f05::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
