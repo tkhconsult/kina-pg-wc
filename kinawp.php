c598155e10d088f1eeea629b7fb3aa8d84e893f3
kch <wilzokch@gmail.com>
2021-06-03 17:43:38 +0800

    update version

--- kinawp.php
+++ kinawp.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Kinabank Payment Gateway
  * Description: WooCommerce Payment Gateway for Kinabank
  * Plugin URI: https://github.com/tkhconsult/kinawp
- * Version: 1.3.1
+ * Version: 1.3.2
  * Author: tkhconsult
  * Text Domain: kinawp
  * Domain Path: /languages
