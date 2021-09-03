2031aafe62a76efc97813b459c2bd269a9c24e27
qqe.app <59642795+qqe-app@users.noreply.github.com>
2021-09-03 23:52:27 +0800

    use difference js file for test mode

--- vendor/tkhconsult/kina-pg-sdk/src/KinaBank/payment-form.php
+++ vendor/tkhconsult/kina-pg-sdk/src/KinaBank/payment-form.php
@@ -11,6 +11,7 @@
  * @var string $acceptUrl
  * @var bool $isHosted
  * @var bool $showAccept
+ * @var bool $testMode
  */
 if($isHosted) { ?>
     <style>
@@ -22,7 +23,7 @@ if($isHosted) { ?>
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
-    <script src="<?php echo $host; ?>/kina/js/kbl-ec.js"></script>
+    <script src="<?php echo $host; ?>/kina/js/kbl-ec<?php echo ($testMode) ? '-test': ''; ?>.js"></script>
     <link href="<?php echo $host; ?>/kina/css/kbl-ec.css" rel="stylesheet">
     <style>
         .fg-row #kblpaymentiframe {
